8d4b5cb7fdeb780a112180928676c8a6296dc5de
duyhung6996 <phamduyhung2002xtc@gmail.com>
2024-01-19 00:05:25 +0700

    First Commit

--- app/src/main/java/com/example/testgit/MainActivity.kt
+++ app/src/main/java/com/example/testgit/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.testgit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tv1 = findViewById<TextView>(R.id.tv_1)
+        tv1.text = "Hello"
     }
 }
