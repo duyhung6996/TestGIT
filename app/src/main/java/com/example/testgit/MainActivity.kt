2539fbb7f43815f67e2e0297b84e220156318952
duyhung6996 <phamduyhung2002xtc@gmail.com>
2024-01-19 00:35:35 +0700

    Second Push

--- app/src/main/java/com/example/testgit/MainActivity.kt
+++ app/src/main/java/com/example/testgit/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val tv1 = findViewById<TextView>(R.id.tv_1)
-        tv1.text = "Hello"
+        tv1.text = "Test 123"
         val tv2 = findViewById<TextView>(R.id.tv_2)
         tv2.text = "ManPD"
     }
